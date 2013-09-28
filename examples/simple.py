51bc22121d13409bacbfb8835db11685da18456d
Sam Kimbrel <kimbrel@me.com>
2013-09-27 21:07:09 -0700

    fix examples

--- examples/simple.py
+++ examples/simple.py
@@ -8,16 +8,19 @@ switch = Switch(__name__)
 @switch.connect('Test')
 def test_handler(request, response):
     response.message(msg="Hello framework monkey!")
+    return response
 
 
 @switch.connect('More test')
 def second_handler(request, response):
     response.message(msg="Look, a second message!")
+    return response
 
 
 @switch.connect('Echo (?P<message>\w+)', re.IGNORECASE)
 def reply_handler(request, response, message):
     response.message(msg="Thanks for your message: {}".format(message))
+    return response
 
 
 if __name__ == '__main__':
